cc9a546b2c968294f27e19ddd9e240e719504f6f
Matteo Salvestrini <matteo.salvestrini@icloud.com>
2022-12-15 11:57:22 +0100

    chore: restore export

--- src/index.ts
+++ src/index.ts
@@ -22,14 +22,14 @@ import * as data from './data';
   const accessToken = await token();
   d(`Access token '${accessToken}'`);
 
-  // try {
-  //   const result = await data.getRecursosFiscalizados(accessToken);
-  //   const promises = data.publishRecursosFiscalizados('indicador_x', result);
-  //   await Promise.all(promises.map(throttle));
-  // } catch (e: any) {
-  //   console.error('Unable to publish conta prestada', e.message);
-  //   return;
-  // }
+  try {
+    const result = await data.getRecursosFiscalizados(accessToken);
+    const promises = data.publishRecursosFiscalizados('indicador_x', result);
+    await Promise.all(promises.map(throttle));
+  } catch (e: any) {
+    console.error('Unable to publish conta prestada', e.message);
+    return;
+  }
 
   try {
     const result = await data.getProcessosDecididosNaPrevia(accessToken);
@@ -53,11 +53,11 @@ import * as data from './data';
     return;
   }
 
-  // try {
-  //   const result = await data.getContasPrestadas(accessToken);
-  //   const promises = data.publishContasPrestadas('indicador_3', result);
-  //   await Promise.all(promises.map(throttle));
-  // } catch (e: any) {
-  //   console.error('Unable to publish conta gerencia', e.message);
-  // }
+  try {
+    const result = await data.getContasPrestadas(accessToken);
+    const promises = data.publishContasPrestadas('indicador_3', result);
+    await Promise.all(promises.map(throttle));
+  } catch (e: any) {
+    console.error('Unable to publish conta gerencia', e.message);
+  }
 })();
