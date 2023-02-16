9179fe276c86be21aea0fa8a4c615285a1f0f68a
Matteo Salvestrini <matteo.salvestrini@icloud.com>
2023-02-16 16:26:46 +0100

    chore: remove console log

--- src/drupal.ts
+++ src/drupal.ts
@@ -1,14 +1,8 @@
 import DrupalSDK from 'drupal-sdk';
 import debug from 'debug';
-import pThrottle from 'p-throttle';
 import config from './config';
 
 const d = debug('pdx:drupal');
-
-const throttle = pThrottle({
-  limit: 2,
-  interval: 3000,
-});
 
 export const Drupal = new DrupalSDK({
   url: config.drupalHost,
@@ -30,20 +24,18 @@ export const upsertContent = async (
   const content = Drupal.get('node', entity);
 
   try {
-    console.log(' GET DATA FOR ', key);
     const result = await content.read('', {
       filter: {title: {value: key}},
     });
-    console.log('RESULT', result);
+
     if (result.data?.length > 0) {
-      console.log('UPDATE', result.data[0].id, JSON.stringify(data, null, 2));
       return await content.update(result.data[0].id, {
         attributes: {
           ...data,
         },
       });
     }
-    console.log('CREATE', JSON.stringify(data, null, 2));
+
     return await content.create({
       attributes: {
         title: key,
